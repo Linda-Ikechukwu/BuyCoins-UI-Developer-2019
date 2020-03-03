f322ca2114e58d58c516a536f699855706b47c5b
Linda Ikechukwu <32891838+Linda-Ikechukwu@users.noreply.github.com>
2020-03-03 10:46:08 +0100

    resolved bug. added both async await and fetch method

--- index.js
+++ index.js
@@ -13,38 +13,38 @@ const nextButton = document.querySelector('#next');
 const state = {};
 
 class Actions {
-  
+    count = 10;
+    array = [];
     constructor(coinsPerPage){
         this.coinsPerPage = coinsPerPage;
-        
     }
     
     async fetchCoins() {
         try{
-            const response = await axios('https://api.coinlore.com/api/tickers/');
-            this.array = response.data.data;
-            console.log(this.array);
+            const response = await fetch('https://api.coinlore.com/api/tickers/');
+            const result = await response.json();
+            this.array = result.data;
+            return this.array;
         }
         catch(error){
            console.log(error)
         }
     }
+    
     nextCoins(){
-        let i =0;
-        i = i + this.coinsPerPage;
-        i = i % this.array;
-        return this.array.slice(i, i+coinsPerPage);
+        this.count = this.count + this.coinsPerPage;
+        console.log(this.count);
+        return this.array.slice(this.count, this.count + this.coinsPerPage);
     }
 
     prevCoins(){
-        let i = 0;
-        i = i - this.coinsPerPage;
-        return this.array.slice(i, i+coinsPerPage);
+        this.count -= this.coinsPerPage;
+        return this.array.slice(this.count, this.count + this.coinsPerPage);
     }
 
     displayButtons(){
-         prevButton.style.visibility = i > 0 ?  "visible" : "hidden";
-         nextButton.style.visibility = i >= 90 ?  "hidden" : "visible";
+        prevButton.style.visibility = this.count > 0 ?  "visible" : "hidden";
+        nextButton.style.visibility = this.count >= (this.array.length - this.coinsPerPage) ?  "hidden" : "visible";
     }
     
     getCoins(coins){
@@ -64,57 +64,104 @@ class Actions {
         tbody.innerHTML = " ";
         button === 'next' ? coins = this.nextCoins() : coins = this.prevCoins();
         this.getCoins(coins);
-        this.displayButtons()
+        this.displayButtons();
     }
     
-    init(){
-        let coinsData = this.array.slice(0,10);
+    init(coins, direction){
+        let coinsData = coins.slice(this.count, this.coinsPerPage);
         this.getCoins(coinsData);
+        this.displayNewCoins(direction);
     }
 }
 
 
+const actions = new Actions(10);
 const newAction = async (direction) => {
-    state.coinData = new Actions(10);
     try{
-      await state.coinData.fetchCoins();
-      //state.coinData.init();
-      state.coinData.displayNewCoins(direction);
+      const coinData = await actions.fetchCoins();
+      actions.init(coinData, direction);
+      //actions.displayButtons();
+      console.log(actions.count);
     }catch(error){
        console.log(error);
     }
     
 }
-newAction();
+newAction(next);
 
-/*const buttonClick = () =>{
-    state.coinsData.displayNewCoins
-    let coins;
-    tbody.innerHTML = " ";
-    button === 'next' ? coins = this.nextCoins() : coins = this.prevCoins();
-    this.getCoins(coins);
-    this.displayButtons()
-}*/
-
-const newAction2 = async (direction) => {
-    state.coinData = new Actions(10);
-    try{
-        await state.coinData.fetchCoins();
-        state.coinData.displayNewCoins(direction);
-    }catch(error){
-        console.log(error);
-    }
-}
-
-
- 
 nextButton.addEventListener('click', () => {
-    newAction('next');
-} );
+newAction('next');
+nextButton.blur();
+});
+
 prevButton.addEventListener('click', () => {
-    newAction('prev');
+ newAction('prev');
+ nextButton.blur();
 } );
 
- 
 
+//Fetch Version, This is faster, I think 
+/*const tbody = document.querySelector('#coins-data');
+const prevButton = document.querySelector('#prev');
+const nextButton = document.querySelector('#next');
+
+
+fetch('https://api.coinlore.com/api/tickers/')
+   .then(response => response.json())
+   .then(coins => {
+       let i = 0;
+       let arrLength = coins.data.length
+       console.log(arrLength);
+       let array = coins.data;
+       coinsData = array.slice(0,10);
+       getCoins();
+
+       function nextCoins(){
+           i = i + 10;
+           return array.slice(i, i+10);
+       }
+
+       function prevCoins(){
+           i = i - 10;
+           return array.slice(i, i+10);
+        }
+
+        function displayButtons(){
+            prevButton.style.visibility = i > 0 ?  "visible" : "hidden";
+            nextButton.style.visibility = i >= 90 ?  "hidden" : "visible";
+        }
+
+       function getCoins(){
+            for(let coin of coinsData){
+                let tableRow = `<tr>
+                                <td>${coin.name}</td>
+                                <td>${coin.symbol}</td>
+                                <td>${coin.price_usd}</td>
+                                <td>${coin.tsupply} ${coin.symbol}</td>
+                                </tr>`
+                tbody.insertAdjacentHTML('beforeend', tableRow);     
+           }
+       }
+
+        nextButton.addEventListener('click', function(e){
+           tbody.innerHTML = " ";
+           coinsData = nextCoins();
+           getCoins();
+           displayButtons()
+
+        })
+
+        prevButton.addEventListener('click', function(e){
+            tbody.innerHTML = " ";
+            coinsData = prevCoins();
+            getCoins();
+            displayButtons()
+
+        })
+
+   }) 
+   .catch(error =>{
+       console.log(error);
+   }); 
+ */
  
